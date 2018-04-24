80ba773df6baae3b01407c25ef51c235d1e4aebb
Rafael da Silva Menna Barreto <rafael.barreto@cwi.com.br>
2018-04-24 09:13:47 -0300

    controller funcionando

--- api/app.py
+++ api/app.py
@@ -1,14 +1,1 @@
-from flask import Flask
-from dominio.entidade.usuario import Usuario
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    user = Usuario("10", "Rafael", "10")
-    return user.login
-
-
-if __name__ == '__main__':
-    app.run()
--- api/dominio/entidade/usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    id = ""
-    login = ""
-    senha = ""
-
-    def __init__(self, id, login, senha):
-        self.id = id
-        self.login = login
-        self.senha = senha
